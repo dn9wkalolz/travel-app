12b8fa9d26133fc95e26bf3d121ca1018b125d23
Alexei <demidenko8888@gmail.com>
2021-03-14 12:01:48 +0300

    refactor: change App

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,9 +1,99 @@
-import React from 'react';
+import { Container } from '@material-ui/core'
+import { useEffect, useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import { userAPI } from './api/api'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import GridLayout from './components/GridLayout/GridLayout'
+import NavigationBar from './components/NavigationBar/NavigationBar'
 
-const App = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);
+function App() {
+  const [data, setData] = useState(null)
+  const [filter, setFilter] = useState('')
+  const [lang, setLang] = useState('en')
 
-export default App;
+  const handleLanguageChange = (event) => {
+    setLang(event.target.value)
+  }
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value)
+    console.log('Filter: ', filter)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await userAPI.getCountries(lang)
+
+      setData(result)
+    }
+
+    fetchData()
+  }, [lang])
+
+  return (
+    <div className='App'>
+      <NavigationBar
+        onChange={handleSearchChange}
+        lang={lang}
+        handleLanguageChange={handleLanguageChange}
+      />
+
+      <Container>
+        <main>
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={() => <Redirect from='/' to='/countries' />}
+            />
+            <Route
+              exact
+              path='/countries'
+              render={() => {
+                return <GridLayout countries={data} filter={filter} />
+              }}
+            />
+            <Route
+              exact
+              path='/countries/:countryId?'
+              render={() => {
+                return (
+                  <div>
+                    <h1>Contry</h1>
+                    <h2>Contry</h2>
+                  </div>
+                )
+              }}
+            />
+            <Route
+              path='*'
+              render={() => (
+                <div>
+                  <h1>404 not found</h1>
+                </div>
+              )}
+            />
+          </Switch>
+        </main>
+      </Container>
+
+      <Footer />
+    </div>
+  )
+}
+
+const AppContainer = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  )
+}
+
+export default AppContainer

c63e019fa9f695dac778cb692a9ed1157f5294c7
Lara <larysa.yavorska@outlook.com>
2021-03-14 19:00:15 +0100

    feat: add password field

---
+++ travel/src/userAuthorization/TextFieldPassword.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { InputAdornment, IconButton } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { TextFieldInput } from './TextFieldInput';
+
+export const TextFieldPassword = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <TextFieldInput
+      name="password"
+      type={showPassword ? 'text' : 'password'}
+      inputProps={{
+        startAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};

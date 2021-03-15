50bc18961dbf9c6fb933fcf338328b6b52728f16
nottombraider <larysa.yavorska@outlook.com>
2021-03-15 22:58:08 +0100

    feat: move authorization from cookies to token

--- travel/src/userAuthorization/LoginForm/loginFormSubmitHandler.js
+++ travel/src/userAuthorization/LoginForm/loginFormSubmitHandler.js
@@ -5,13 +5,17 @@ export const loginFormSubmitHandler = async (formValues) => {
   const body = formValuesEntries.map((item) => item.join('=')).join('&');
 
   try {
-    await fetch(API_OK_TRAVEL_ENDPOINTS.login, {
+    const response = await fetch(API_OK_TRAVEL_ENDPOINTS.login, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
       },
       body,
     });
+
+    const { token } = await response.json();
+
+    sessionStorage.setItem('authToken', token);
   } catch (error) {
     // TO_DO: implement error handler
     // eslint-disable-next-line

d68db3997c4800ac9cc6ff5d11f43a0f460ab4d0
nottombraider <larysa.yavorska@outlook.com>
2021-03-15 23:22:07 +0100

    feat: add form headers

--- travel/src/userAuthorization/LoginForm/LoginForm.jsx
+++ travel/src/userAuthorization/LoginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  FormControl, Button, Box, InputAdornment,
+  FormControl, Button, Box, InputAdornment, Typography,
 } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 import { Form } from 'react-final-form';
@@ -10,43 +10,48 @@ import { TextFieldPassword } from '../TextFieldPassword';
 import { minLengthValidation } from '../formValidation';
 
 export const LoginForm = ({ onSubmit }) => (
-  <Form onSubmit={onSubmit}>
-    {({
-      handleSubmit, invalid, submitting, form,
-    }) => (
 
-      <form onSubmit={(e) => handleSubmit(e).then(form.reset)}>
-        <Box>
-          <FormControl margin="normal">
-            <TextFieldInput
-              name="login"
-              validate={minLengthValidation(4)}
-              inputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Box mx={1.5}>
-                      <AccountCircle />
-                    </Box>
+  <Box>
+    <Typography variant="h5" component="h2">Log in</Typography>
+    <Form onSubmit={onSubmit}>
+      {({
+        handleSubmit, invalid, submitting, form,
+      }) => (
 
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl margin="normal">
-            <TextFieldPassword />
-          </FormControl>
-        </Box>
+        <form onSubmit={(e) => handleSubmit(e).then(form.reset)}>
+          <Box>
+            <FormControl margin="normal">
+              <TextFieldInput
+                name="login"
+                validate={minLengthValidation(4)}
+                inputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Box mx={1.5}>
+                        <AccountCircle />
+                      </Box>
 
-        <Box my={1}>
-          <Button color="primary" variant="contained" type="submit" disabled={invalid || submitting}>Log in</Button>
-        </Box>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+          </Box>
+          <Box>
+            <FormControl margin="normal">
+              <TextFieldPassword />
+            </FormControl>
+          </Box>
 
-      </form>
-    )}
-  </Form>
+          <Box my={1}>
+            <Button color="primary" variant="contained" type="submit" disabled={invalid || submitting}>Log in</Button>
+          </Box>
+
+        </form>
+      )}
+    </Form>
+
+  </Box>
 );
 
 LoginForm.propTypes = {
--- travel/src/userAuthorization/RegistrationForm/RegistrationForm.jsx
+++ travel/src/userAuthorization/RegistrationForm/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  FormControl, Button, Box, InputAdornment,
+  FormControl, Button, Box, InputAdornment, Typography,
 } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 import { Form } from 'react-final-form';
@@ -10,50 +10,55 @@ import { TextFieldPassword } from '../TextFieldPassword';
 import { minLengthValidation } from '../formValidation';
 
 export const RegistrationForm = ({ onSubmit }) => (
-  <Form onSubmit={onSubmit}>
-    {({
-      handleSubmit, invalid, submitting, form,
-    }) => (
-      <form onSubmit={(e) => handleSubmit(e).then(form.reset)}>
-        <Box>
-          <FormControl margin="normal">
-            <TextFieldInput
-              name="login"
-              validate={minLengthValidation(4)}
-              inputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Box mx={1.5}>
-                      <AccountCircle />
-                    </Box>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl margin="normal">
-            <TextFieldPassword />
-          </FormControl>
-        </Box>
+  <Box>
+    <Typography variant="h5" component="h2">Registration</Typography>
+    <Form onSubmit={onSubmit}>
+      {({
+        handleSubmit, invalid, submitting, form,
+      }) => (
 
-        {/* TO_DO: add field for uploading user photo */}
+        <form onSubmit={(e) => handleSubmit(e).then(form.reset)}>
+          <Box>
+            <FormControl margin="normal">
+              <TextFieldInput
+                name="login"
+                validate={minLengthValidation(4)}
+                inputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Box mx={1.5}>
+                        <AccountCircle />
+                      </Box>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+          </Box>
+          <Box>
+            <FormControl margin="normal">
+              <TextFieldPassword />
+            </FormControl>
+          </Box>
 
-        <Box my={1}>
-          <Button
-            color="primary"
-            variant="contained"
-            type="submit"
-            disabled={invalid || submitting}
-          >
-            Register
-          </Button>
-        </Box>
+          {/* TO_DO: add field for uploading user photo */}
 
-      </form>
-    )}
-  </Form>
+          <Box my={1}>
+            <Button
+              color="primary"
+              variant="contained"
+              type="submit"
+              disabled={invalid || submitting}
+            >
+              Register
+            </Button>
+          </Box>
+
+        </form>
+      )}
+
+    </Form>
+  </Box>
 
 );
 

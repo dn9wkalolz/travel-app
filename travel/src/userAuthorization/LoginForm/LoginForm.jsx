c31d4ec3ebbfbc08846cd2be67a7246bfe2ac945
nottombraider <larysa.yavorska@outlook.com>
2021-03-17 14:57:26 +0100

    feat: add redirect to main pae after login

--- travel/src/userAuthorization/LoginForm/LoginForm.jsx
+++ travel/src/userAuthorization/LoginForm/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
   FormControl, Button, Box, InputAdornment, Typography,
@@ -9,50 +10,52 @@ import { TextFieldInput } from '../TextFieldInput';
 import { TextFieldPassword } from '../TextFieldPassword';
 import { minLengthValidation } from '../formValidation';
 
-export const LoginForm = ({ onSubmit }) => (
+export const LoginForm = ({ onSubmit }) => {
+  const history = useHistory();
+  return (
+    <Box>
+      <Typography variant="h5" component="h2">Log in</Typography>
+      <Form onSubmit={onSubmit}>
+        {({
+          handleSubmit, invalid, submitting, form,
+        }) => (
 
-  <Box>
-    <Typography variant="h5" component="h2">Log in</Typography>
-    <Form onSubmit={onSubmit}>
-      {({
-        handleSubmit, invalid, submitting, form,
-      }) => (
+          <form onSubmit={(e) => handleSubmit(e).then(form.reset && history.push('/'))}>
+            <Box>
+              <FormControl margin="normal">
+                <TextFieldInput
+                  name="login"
+                  validate={minLengthValidation(4)}
+                  inputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Box mx={1.5}>
+                          <AccountCircle />
+                        </Box>
 
-        <form onSubmit={(e) => handleSubmit(e).then(form.reset)}>
-          <Box>
-            <FormControl margin="normal">
-              <TextFieldInput
-                name="login"
-                validate={minLengthValidation(4)}
-                inputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Box mx={1.5}>
-                        <AccountCircle />
-                      </Box>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl margin="normal">
+                <TextFieldPassword />
+              </FormControl>
+            </Box>
 
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </FormControl>
-          </Box>
-          <Box>
-            <FormControl margin="normal">
-              <TextFieldPassword />
-            </FormControl>
-          </Box>
+            <Box my={1}>
+              <Button color="primary" variant="contained" type="submit" disabled={invalid || submitting}>Log in</Button>
+            </Box>
 
-          <Box my={1}>
-            <Button color="primary" variant="contained" type="submit" disabled={invalid || submitting}>Log in</Button>
-          </Box>
+          </form>
+        )}
+      </Form>
 
-        </form>
-      )}
-    </Form>
-
-  </Box>
-);
+    </Box>
+  );
+};
 
 LoginForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
